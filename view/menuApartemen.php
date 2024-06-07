ebe8a11f79d42a2f1b5e65947a45a612f036da53
GekYutaa <yutta230925@gmail.com>
2024-06-07 16:38:34 +0800

    menambahkan menu apartemen dan gambar bagian dari apartemen

---
+++ view/menuApartemen.php
@@ -0,0 +1,99 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Menu Villa HARUM</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <style>
+        .card-img-top {
+            height: 200px;
+            object-fit: cover;
+        }
+        .villa-list h2 {
+            text-align: center;
+            margin-bottom: 50px;
+        }
+    </style>
+</head>
+<body>
+    <header class="bg-white shadow-sm">
+        <div class="container d-flex justify-content-between align-items-center py-3">
+            <img src="../img/logo.jpg" alt="Harum Logo" class="img-fluid" style="height: 40px;">
+            <nav class="d-flex">
+                <a href="#" class="text-dark mx-2">Hotel</a>
+                <a href="#" class="text-dark mx-2">Villa</a>
+                <a href="#" class="text-dark mx-2">Apartemen</a>
+            </nav>
+            <button class="btn btn-primary">Daftar</button>
+        </div>
+    </header>
+
+    <main>
+        <section class="villa-list py-5">
+            <div class="container">
+                <h2>Recommended Apartement</h2>
+                <div class="row">
+                    <div class="col-md-4 mb-4">
+                        <div class="card">
+                            <img src="../img/imageForMenuTampilan/therooms-apartemen.webp" class="card-img-top" alt="The Rooms Apartment Bali by ARM Hospitality">
+                            <div class="card-body">
+                                <h4 class="card-title">The Rooms Apartment Bali by ARM Hospitality</h4>
+                                <h6 class="card-subtitle mb-2 text-muted">
+                                    <i class="fa fa-map-marker" aria-hidden="true"></i>
+                                    Jl Gunung Soputan, mahendradatta , kepuh segina, Denpasar Barat, Denpasar, Bali, Indonesia, 80117
+                                </h6>
+                                <p class="card-text">Start From IDR 412.500</p>
+                                <a href="#" class="btn btn-primary">Detail</a>
+                            </div>
+                        </div>  
+                    </div>
+                    <div class="col-md-4 mb-4">
+                        <div class="card">
+                            <img src="../img/imageForMenuTampilan/candraloka-apartemen.webp" class="card-img-top" alt="Candra Loka Studio & Apartment by The Loka">
+                            <div class="card-body">
+                                <h4 class="card-title">Candra Loka Studio & Apartment by The Loka</h4>
+                                <h6 class="card-subtitle mb-2 text-muted">
+                                    <i class="fa fa-map-marker" aria-hidden="true"></i>
+                                    Jl. Muding Indah Gg. Palm no. 8, Kerobokan, Kuta, Denpasar, Bali, Canggu, Kuta Utara, Bali, Indonesia, 80363
+                                </h6>
+                                <p class="card-text">Start From IDR 406.686</p>
+                                <a href="#" class="btn btn-primary">Detail</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-4 mb-4">
+                        <div class="card">
+                            <img src="../img/imageForMenuTampilan/balitrueliving-apartemen.webp" class="card-img-top" alt="Bali True Living Apartment">
+                            <div class="card-body">
+                                <h4 class="card-title">Bali True Living Apartment</h4>
+                                <h6 class="card-subtitle mb-2 text-muted">
+                                    <i class="fa fa-map-marker" aria-hidden="true"></i>
+                                    Jl Imam Bonjol Perum Budha Cemeng Ukir No 1, Denpasar Barat, Denpasar, Bali, Indonesia, 80119
+                                </h6>
+                                <p class="card-text">Start From IDR 423.000</p>
+                                <a href="#" class="btn btn-primary">Detail</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+
+    <footer class="bg-white py-4">
+        <div class="container d-flex justify-content-between align-items-center">
+        <img src="../img/logo.jpg" alt="" class="img-fluid" style="height: 40px;">
+            <span class="text-dark">©2024 - Harum</span>
+            <div class="d-flex">
+                <a href="#" class="text-dark mx-2">Tim Developer</a>
+                <a href="#" class="text-dark mx-2">Kontak Kami</a>
+                <a href="#" class="text-dark mx-2">About</a>
+            </div>
+        </div>
+    </footer>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
